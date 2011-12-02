cb376a2120eba9479d20cc0e96e6fa2628804154
quake wang <quake.wang@gmail.com>
2011-12-02 11:57:56 +0800

    add install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+mkdir tmp
+cd tmp
+git clone https://github.com/sunspot/sunspot.git
+wget http://mmseg4j.googlecode.com/files/mmseg4j-1.8.3.zip
+unzip mmseg4j-1.8.3.zip -d mmseg4j
+cd ..
+cp tmp/sunspot/sunspot_solr/solr sunspot_solr_mmseg4j -r
+cp tmp/mmseg4j/data sunspot_solr_mmseg4j/solr/dict -r
+mkdir -p tmp/WEB-INF/lib
+cp tmp/mmseg4j/mmseg4j-all-1.8.3.jar tmp/WEB-INF/lib
+sed 's,class="solr.StandardTokenizerFactory",class="com.chenlb.mmseg4j.solr.MMSegTokenizerFactory" mode="max-word" dicPath="dict",g' -i sunspot_solr_mmseg4j/solr/conf/schema.xml
+jar uf sunspot_solr_mmseg4j/webapps/solr.war -C tmp WEB-INF/lib/mmseg4j-all-1.8.3.jar
+rm -rf tmp
+echo "******************"
+echo "** install done **"
+echo "******************"
+echo "cd sunspot_solr_mmseg4j and run 'java -jar start.jar' to start jetty"
+echo "visit http://localhost:8983/solr/ to verify result" 
