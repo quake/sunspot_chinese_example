0041ebb72b6deab9e725460a797224a47931a2b1
quake wang <quake.wang@gmail.com>
2013-08-19 15:43:22 +0800

    upgrade to sunspot 2.0.0.pre.130115 and mmseg4j-1.8.5

--- README.md
+++ README.md
@@ -2,21 +2,25 @@
 
 执行install.sh脚本以后，会在本目录下面生成一个sunspot_solr_mmseg4j目录，这里包括了jetty，solr的war文件，以及mmseg4j的集成，你只需要执行java -jar start.jar，就能够启动一个solr server。
 
+启动后，用浏览器访问 http://localhost:8983/solr/admin/analysis.jsp
+
+选择Field为type，输入text。 Field value (Index) 输入 “研究生研究生命科学” 。点击Analyze，如果输出“研究|生|研究|生命|科学”，就说明mmseg4j + sunspot 已经build成功。
+
 sunspot_solr_mmseg4j这个目录可以拷贝到任意地方，作为提供全文索引的服务器。
 
 对于高级的配置，你可以修改sunspot_solr_mmseg4j/solr/conf/schema.xml文件
 
-比如添加index和query的分析器，让查询也需要走分词器：
+比如添加index和query的分析器，让查询也需要走分词器，或者更改mmseg4j的分词模式（build好的默认是max-word）
 
 ```xml
     <fieldType name="text" class="solr.TextField" omitNorms="false">
       <analyzer type="index">
-        <tokenizer class="com.chenlb.mmseg4j.solr.MMSegTokenizerFactory" mode="max-word" dicPath="dict"/>
+        <tokenizer class="com.chenlb.mmseg4j.solr.MMSegTokenizerFactory" mode="max-word" dicPath="mmseg4j_dict"/>
         <filter class="solr.StandardFilterFactory"/>
         <filter class="solr.LowerCaseFilterFactory"/>
       </analyzer>
       <analyzer type="query">
-        <tokenizer class="com.chenlb.mmseg4j.solr.MMSegTokenizerFactory" mode="max-word" dicPath="dict"/>
+        <tokenizer class="com.chenlb.mmseg4j.solr.MMSegTokenizerFactory" mode="max-word" dicPath="mmseg4j_dict"/>
         <filter class="solr.StandardFilterFactory"/>
         <filter class="solr.LowerCaseFilterFactory"/>
         <filter class="solr.PositionFilterFactory" />
@@ -24,4 +28,4 @@ sunspot_solr_mmseg4j这个目录可以拷贝到任意地方，作为提供全文
     </fieldType>
 ```
 
-或者在dict目录下面添加更多的自定义词库。
+或者在solr/mmseg4j_dict目录下面添加更多的自定义词库。
--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
 #! /bin/sh
 mkdir tmp
 cd tmp
-wget https://github.com/sunspot/sunspot/tarball/v2.0.0.pre.111215
-tar zxf v2.0.0.pre.111215
-mv sunspot-sunspot-6489d63/ sunspot
-wget http://mmseg4j.googlecode.com/files/mmseg4j-1.8.3.zip
-unzip mmseg4j-1.8.3.zip -d mmseg4j
+wget https://github.com/sunspot/sunspot/archive/v2.0.0.pre.130115.zip
+unzip v2.0.0.pre.130115.zip
+mv sunspot-2.0.0.pre.130115 sunspot
+wget http://mmseg4j.googlecode.com/files/mmseg4j-1.8.5.zip
+unzip mmseg4j-1.8.5.zip -d mmseg4j
+cp sunspot/sunspot_solr/solr ../sunspot_solr_mmseg4j -r
+mkdir -p WEB-INF/lib
+cp mmseg4j/dist/*-1.8.5.jar WEB-INF/lib
+cp mmseg4j/data/ ../sunspot_solr_mmseg4j/solr/mmseg4j_dict/ -r
+sed 's,class="solr.StandardTokenizerFactory",class="com.chenlb.mmseg4j.solr.MMSegTokenizerFactory" mode="max-word" dicPath="mmseg4j_dict",g' -i ../sunspot_solr_mmseg4j/solr/conf/schema.xml
+zip -u ../sunspot_solr_mmseg4j/webapps/solr.war WEB-INF/lib/*.jar
 cd ..
-cp tmp/sunspot/sunspot_solr/solr sunspot_solr_mmseg4j -r
-cp tmp/mmseg4j/data sunspot_solr_mmseg4j/solr/dict -r
-mkdir -p tmp/WEB-INF/lib
-cp tmp/mmseg4j/mmseg4j-all-1.8.3.jar tmp/WEB-INF/lib
-sed 's,class="solr.StandardTokenizerFactory",class="com.chenlb.mmseg4j.solr.MMSegTokenizerFactory" mode="max-word" dicPath="dict",g' -i sunspot_solr_mmseg4j/solr/conf/schema.xml
-jar uf sunspot_solr_mmseg4j/webapps/solr.war -C tmp WEB-INF/lib/mmseg4j-all-1.8.3.jar
 rm -rf tmp
 echo "******************"
 echo "** install done **"
 echo "******************"
 echo "cd sunspot_solr_mmseg4j and run 'java -jar start.jar' to start jetty"
-echo "visit http://localhost:8983/solr/ to verify result" 
+echo "visit http://localhost:8983/solr/ to verify result"
